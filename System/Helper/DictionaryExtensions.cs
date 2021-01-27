f53c0dc8aab5a52026bd29f917555a0fe0b1b487
Frank Mühlschlegel <frank@fluentchange.com>
2021-01-27 17:17:44 +0100

    added Dictionary copy extension

---
+++ System/Helper/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentChange.Extensions.System.Helper
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<string, string> Copy(this Dictionary<string, string> routeParams)
+        {
+            var copy = new Dictionary<string, string>();
+            foreach (var p in routeParams)
+            {
+                copy.Add(p.Key, p.Value);
+            }
+            return copy;
+        }
+    }
+}
