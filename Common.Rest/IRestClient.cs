5122f074db0e365d56edda4e08b5783f56276379
Frank Mühlschlegel <frank@fluentchange.com>
2021-01-27 14:14:02 +0100

    improved with IRestClient

---
+++ Common.Rest/IRestClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FluentChange.Extensions.Common.Rest
+{
+    public interface IRestClient
+    {
+        T Delete<T>(string route, Dictionary<string, string> parameters = null);
+        T Get<T>(string route, Dictionary<string, string> parameters = null);
+        T Post<T>(string route, object requestBody, Dictionary<string, string> parameters = null);
+        T Put<T>(string route, object requestBody, Dictionary<string, string> parameters = null);
+    }
+}
