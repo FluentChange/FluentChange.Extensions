0faf8254736700cdfba5a983e270f3ee59355fa0
Frank Mühlschlegel <frank@fluentchange.com>
2021-01-27 14:07:13 +0100

    little improvement with rest routes / id pattern

---
+++ Common.Rest/Routes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentChange.Extensions.Common.Rest
+{
+    public static class Routes
+    {
+        public const string OptionalId = "/{id?}";
+    }
+}
