97d6f88ca8b202643417de3325726ba6c7580415
Frank Mühlschlegel <frank@fluentchange.com>
2021-10-21 16:21:10 +0200

    extended datetime helpers

--- System/Helper/DateTimeHelper.cs
+++ System/Helper/DateTimeHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FluentChange.Extensions.System.Helper
@@ -67,5 +68,71 @@ namespace FluentChange.Extensions.System.Helper
         }
 
 
+        public static IEnumerable<DateTime> FilterNull(this IEnumerable<DateTime?> dates)
+        {
+            return dates.Where(d => d.HasValue).Select(d => d.Value);
+        }
+
+        public static DateTime? MinOrNull(this IEnumerable<DateTime> dates)
+        {
+            if (dates.Count() > 0)
+            {
+                return dates.Min();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        // TODO: move this to extensions
+        public static DateTime? MaxOrNull(this IEnumerable<DateTime> dates)
+        {
+            if (dates.Count() > 0)
+            {
+                return dates.Max();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static int Quarter(this DateTime date)
+        {
+            return GetQuarter(date.Month);
+        }
+
+        public static int GetQuarter(int month)
+        {
+            if (month < 1 || month > 12) throw new ArgumentOutOfRangeException();
+            if (month >= 4 && month <= 6)
+                return 1;
+            else if (month >= 7 && month <= 9)
+                return 2;
+            else if (month >= 10 && month <= 12)
+                return 3;
+            else
+                return 4;
+        }
+
+        public static int FirstMonthInQuarter(int quarter)
+        {
+            if (quarter < 1 || quarter > 4) throw new ArgumentOutOfRangeException();
+            if (quarter == 1) return 1;
+            else if (quarter == 2) return 4;
+            else if (quarter == 3) return 7;
+            else return 10;
+        }
+
+        public static int LastMonthInQuarter(int quarter)
+        {
+            if (quarter < 1 || quarter > 4) throw new ArgumentOutOfRangeException();
+            if (quarter == 1) return 3;
+            else if (quarter == 2) return 6;
+            else if (quarter == 3) return 9;
+            else return 12;
+        }
     }
 }
