788b5db056094dc60f79a2510fba387174f93f23
Frank Mühlschlegel <frank@fluentchange.com>
2022-01-12 17:34:58 +0100

    dependency upgrade

--- SampleFunctions.Tests/Clients/CRUDLFunctionsApiClient.cs
+++ SampleFunctions.Tests/Clients/CRUDLFunctionsApiClient.cs
@@ -10,23 +10,23 @@ namespace SampleFunctions.Tests
     {
         private readonly IRestClient rest;
 
-        public GenericCRUDLApi<Todo> Sample1Todos { get; private set; }
-        public GenericCRUDLApi<Todo> Sample2Todos { get; private set; }
-        public GenericCRUDLApi<Event> Sample3Events { get; private set; }
-        public GenericCRUDLApi<Event> Sample4Events { get; private set; }
-        public GenericCRUDLApi<Event> Sample5Events { get; private set; }
-        public WrappedGenericCRUDLApi<Product> Sample6Products { get; private set; }
-        public WrappedGenericCRUDLApi<ApiProduct> Sample7Products { get; private set; }
+        public GenericCRUDLWithIdApi<Todo> Sample1Todos { get; private set; }
+        public GenericCRUDLWithIdApi<Todo> Sample2Todos { get; private set; }
+        public GenericCRUDLWithIdApi<Event> Sample3Events { get; private set; }
+        public GenericCRUDLWithIdApi<Event> Sample4Events { get; private set; }
+        public GenericCRUDLWithIdApi<Event> Sample5Events { get; private set; }
+        public WrappedGenericCRUDLWithIdApi<Product> Sample6Products { get; private set; }
+        public WrappedGenericCRUDLWithIdApi<ApiProduct> Sample7Products { get; private set; }
         public CRUDLFunctionsApiClient(IRestClient rest)
         {
             this.rest = rest;
-            Sample1Todos = new GenericCRUDLApi<Todo>(rest, "crudl/sample1/todos", new Dictionary<string, object>());
-            Sample2Todos = new GenericCRUDLApi<Todo>(rest, "crudl/sample2/todos", new Dictionary<string, object>());
-            Sample3Events = new GenericCRUDLApi<Event>(rest, "crudl/sample3/events", new Dictionary<string, object>());
-            Sample4Events = new GenericCRUDLApi<Event>(rest, "crudl/sample4/events", new Dictionary<string, object>());
-            Sample5Events = new GenericCRUDLApi<Event>(rest, "crudl/sample5/events", new Dictionary<string, object>());
-            Sample6Products = new WrappedGenericCRUDLApi<Product>(rest, "crudl/sample6/products", new Dictionary<string, object>());  
-            Sample7Products = new WrappedGenericCRUDLApi<ApiProduct>(rest, "crudl/sample7/products", new Dictionary<string, object>());     
+            Sample1Todos = new GenericCRUDLWithIdApi<Todo>(rest, "crudl/sample1/todos", new Dictionary<string, object>());
+            Sample2Todos = new GenericCRUDLWithIdApi<Todo>(rest, "crudl/sample2/todos", new Dictionary<string, object>());
+            Sample3Events = new GenericCRUDLWithIdApi<Event>(rest, "crudl/sample3/events", new Dictionary<string, object>());
+            Sample4Events = new GenericCRUDLWithIdApi<Event>(rest, "crudl/sample4/events", new Dictionary<string, object>());
+            Sample5Events = new GenericCRUDLWithIdApi<Event>(rest, "crudl/sample5/events", new Dictionary<string, object>());
+            Sample6Products = new WrappedGenericCRUDLWithIdApi<Product>(rest, "crudl/sample6/products", new Dictionary<string, object>());  
+            Sample7Products = new WrappedGenericCRUDLWithIdApi<ApiProduct>(rest, "crudl/sample7/products", new Dictionary<string, object>());     
         }
     }
 }
