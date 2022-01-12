7ea14c46957d49da57008a0bfa937c92e19db481
Frank Mühlschlegel <frank@fluentchange.com>
2022-01-12 01:31:19 +0100

    improvements for GenericApi's to support only RUD and CL without the other operations

--- Common.Rest/GenericCRUDLApi.cs
+++ Common.Rest/GenericCRUDLApi.cs
@@ -6,17 +6,47 @@ using System.Threading.Tasks;
 
 namespace FluentChange.Extensions.Common.Rest
 {
-    public class WrappedGenericCRUDApi<T> where T : new()
-    {
-        private readonly IRestClient rest;
-        private readonly string route;
-        private Dictionary<string, string> routeParams;
 
-        public WrappedGenericCRUDApi(IRestClient rest, string route, Dictionary<string, string> routeParams)
+    public abstract class BaseAbstractApi<T> where T : new()
+    {
+        protected readonly IRestClient rest;
+        protected readonly string route;
+        protected Dictionary<string, string> routeParams;
+
+        public BaseAbstractApi(IRestClient rest, string route, Dictionary<string, string> routeParams)
         {
             this.rest = rest;
             this.route = route;
             this.routeParams = routeParams;
+        }
+    }
+
+    public class WrappedGenericCLApi<T> : BaseAbstractApi<T> where T : IEntityWithId, new()
+    {
+        public WrappedGenericCLApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
+        {
+        }
+
+        public async Task<SingleResponse<T>> Create(T entity)
+        {
+            var request = new SingleRequest<T>();
+            request.Data = entity;
+            var paramsDic = routeParams.Copy();
+
+            return await rest.Post<SingleResponse<T>>(route, request, paramsDic);
+        }
+
+        public async Task<MultiResponse<T>> List()
+        {
+            var paramsDic = routeParams.Copy();
+            return await rest.Get<MultiResponse<T>>(route, paramsDic);
+        }
+
+    }
+    public class WrappedGenericRUDApi<T> : BaseAbstractApi<T> where T : new()
+    {
+        public WrappedGenericRUDApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
+        {
         }
 
         public async Task<SingleResponse<T>> Create(T entity)
@@ -30,7 +60,7 @@ namespace FluentChange.Extensions.Common.Rest
 
         public async Task<SingleResponse<T>> Read()
         {
-            var paramsDic = routeParams.Copy();        
+            var paramsDic = routeParams.Copy();
             var read = rest.Get<SingleResponse<T>>(route, paramsDic);
             return await read;
         }
@@ -40,36 +70,59 @@ namespace FluentChange.Extensions.Common.Rest
             var request = new SingleRequest<T>();
             request.Data = entity;
             var paramsDic = routeParams.Copy();
-   
+
             return await rest.Put<SingleResponse<T>>(route, request, paramsDic);
         }
-
-        public async Task<Response> Delete()
-        {
-            var paramsDic = routeParams.Copy();   
-            return await rest.Delete<Response>(route, paramsDic);
-        }      
     }
-    public class WrappedGenericCRUDLApi<T> where T :  IEntityWithId, new()
+    public class WrappedGenericCRUDApi<T> : BaseAbstractApi<T> where T : new()
     {
-        private readonly IRestClient rest;
-        private readonly string route;
-        private Dictionary<string, string> routeParams;
-
-        public WrappedGenericCRUDLApi(IRestClient rest, string route, Dictionary<string, string> routeParams)
+        public WrappedGenericCRUDApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
         {
-            this.rest = rest;
-            this.route = route;
-            this.routeParams = routeParams;   
         }
-
 
         public async Task<SingleResponse<T>> Create(T entity)
         {
             var request = new SingleRequest<T>();
             request.Data = entity;
             var paramsDic = routeParams.Copy();
-             
+
+            return await rest.Post<SingleResponse<T>>(route, request, paramsDic);
+        }
+
+        public async Task<SingleResponse<T>> Read()
+        {
+            var paramsDic = routeParams.Copy();
+            var read = rest.Get<SingleResponse<T>>(route, paramsDic);
+            return await read;
+        }
+
+        public async Task<SingleResponse<T>> Update(T entity)
+        {
+            var request = new SingleRequest<T>();
+            request.Data = entity;
+            var paramsDic = routeParams.Copy();
+
+            return await rest.Put<SingleResponse<T>>(route, request, paramsDic);
+        }
+
+        public async Task<Response> Delete()
+        {
+            var paramsDic = routeParams.Copy();
+            return await rest.Delete<Response>(route, paramsDic);
+        }
+    }
+    public class WrappedGenericCRUDLApi<T> : BaseAbstractApi<T> where T : IEntityWithId, new()
+    {
+        public WrappedGenericCRUDLApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
+        {
+        }
+
+        public async Task<SingleResponse<T>> Create(T entity)
+        {
+            var request = new SingleRequest<T>();
+            request.Data = entity;
+            var paramsDic = routeParams.Copy();
+
             return await rest.Post<SingleResponse<T>>(route, request, paramsDic);
         }
 
@@ -80,8 +133,6 @@ namespace FluentChange.Extensions.Common.Rest
             var read = rest.Get<SingleResponse<T>>(route + Routes.PatternId, paramsDic);
             return await read;
         }
-
-
 
         public async Task<SingleResponse<T>> Update(T entity)
         {
@@ -100,26 +151,62 @@ namespace FluentChange.Extensions.Common.Rest
 
             return await rest.Delete<Response>(route + Routes.PatternId, paramsDic);
         }
+
         public async Task<MultiResponse<T>> List()
         {
             var paramsDic = routeParams.Copy();
             return await rest.Get<MultiResponse<T>>(route, paramsDic);
         }
-
-
     }
 
-    public class GenericCRUDApi<T> where T : new()
-    {
-        private readonly IRestClient rest;
-        private readonly string route;
-        private Dictionary<string, string> routeParams;
 
-        public GenericCRUDApi(IRestClient rest, string route, Dictionary<string, string> routeParams)
+    public class GenericCLApi<T> : BaseAbstractApi<T> where T : IEntityWithId, new()
+    {
+        public GenericCLApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
         {
-            this.rest = rest;
-            this.route = route;
-            this.routeParams = routeParams;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            var paramsDic = routeParams.Copy();
+            return await rest.Post<T>(route, entity, paramsDic);
+        }
+               
+        public async Task<IEnumerable<T>> List()
+        {
+            var paramsDic = routeParams.Copy();
+            return await rest.Get<IEnumerable<T>>(route, paramsDic);
+        }
+    }
+    public class GenericRUDApi<T> : BaseAbstractApi<T> where T : new()
+    {
+        public GenericRUDApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
+        {
+        }
+
+        public async Task<T> Read()
+        {
+            var paramsDic = routeParams.Copy();
+            return await rest.Get<T>(route, paramsDic);
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            var paramsDic = routeParams.Copy();
+            return await rest.Put<T>(route, entity, paramsDic);
+        }
+
+        public async Task Delete()
+        {
+            var paramsDic = routeParams.Copy();
+            await rest.Delete<T>(route, paramsDic);
+        }
+
+    }
+    public class GenericCRUDApi<T> : BaseAbstractApi<T> where T : new()
+    {
+        public GenericCRUDApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
+        {  
         }
 
         public async Task<T> Create(T entity)
@@ -147,19 +234,12 @@ namespace FluentChange.Extensions.Common.Rest
         }
 
     }
-    public class GenericCRUDLApi<T> where T : IEntityWithId, new()
+    public class GenericCRUDLApi<T> : BaseAbstractApi<T> where T : IEntityWithId, new()
     {
-        private readonly IRestClient rest;
-        private readonly string route;
-        private Dictionary<string, string> routeParams;
-        
-        public GenericCRUDLApi(IRestClient rest, string route, Dictionary<string, string> routeParams)
+        public GenericCRUDLApi(IRestClient rest, string route, Dictionary<string, string> routeParams) : base(rest, route, routeParams)
         {
-            this.rest = rest;
-            this.route = route;
-            this.routeParams = routeParams;    
         }
-              
+
 
         public async Task<T> Create(T entity)
         {
@@ -169,17 +249,17 @@ namespace FluentChange.Extensions.Common.Rest
 
         public async Task<T> Read(Guid id)
         {
-            var paramsDic = routeParams.Copy();       
-            paramsDic.Add(Routes.ParamNameId, id.ToString());      
+            var paramsDic = routeParams.Copy();
+            paramsDic.Add(Routes.ParamNameId, id.ToString());
 
             return await rest.Get<T>(route + Routes.PatternId, paramsDic);
-        }              
+        }
 
         public async Task<T> Update(T entity)
-        {         
+        {
             var paramsDic = routeParams.Copy();
             paramsDic.Add(Routes.ParamNameId, entity.Id.ToString());
- 
+
             return await rest.Put<T>(route + Routes.PatternId, entity, paramsDic);
         }
 
@@ -187,7 +267,7 @@ namespace FluentChange.Extensions.Common.Rest
         {
             var paramsDic = routeParams.Copy();
             paramsDic.Add(Routes.ParamNameId, id.ToString());
-   
+
             await rest.Delete<T>(route + Routes.PatternId, paramsDic);
         }
         public async Task<IEnumerable<T>> List()
@@ -195,8 +275,6 @@ namespace FluentChange.Extensions.Common.Rest
             var paramsDic = routeParams.Copy();
             return await rest.Get<IEnumerable<T>>(route, paramsDic);
         }
-
-      
     }
 
 }
