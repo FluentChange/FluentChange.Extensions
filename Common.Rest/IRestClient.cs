e5b24fc23d997cab6842df27a2cf9a2fb9fea17b
Frank Mühlschlegel <frank@fluentchange.com>
2022-02-15 10:51:40 +0100

    improved rest client interface with PostFile

--- Common.Rest/IRestClient.cs
+++ Common.Rest/IRestClient.cs
@@ -8,6 +8,7 @@ namespace FluentChange.Extensions.Common.Rest
         Task<T> Delete<T>(string route, Dictionary<string, object> parameters = null);
         Task<T> Get<T>(string route, Dictionary<string, object> parameters = null);
         Task<T> Post<T>(string route, object requestBody, Dictionary<string, object> parameters = null);
+        Task<T> PostFile<T>(string route, string filePath, Dictionary<string, object> parameters = null);
         Task<T> Put<T>(string route, object requestBody, Dictionary<string, object> parameters = null);
     }
 }
