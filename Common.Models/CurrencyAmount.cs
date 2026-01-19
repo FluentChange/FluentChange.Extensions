4605d034ea3e89f22c44225f383c5161319ae959
Frank Mühlschlegel <frank@fluentchange.com>
2026-01-20 00:58:44 +0100

    new models

---
+++ Common.Models/CurrencyAmount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FluentChange.Extensions.Common.Models
+{
+    public class CurrencyAmount
+    {
+        public required string Currency { get; set; }
+        public decimal Amount { get; set; }
+
+
+        public CurrencyAmount Mutliply(double multiplier)
+        {
+            return new CurrencyAmount { Currency = Currency, Amount = Amount * Convert.ToDecimal(multiplier) };
+        }
+        public CurrencyAmount Mutliply(decimal multiplier)
+        {
+            return new CurrencyAmount { Currency = Currency, Amount = Amount * multiplier };
+        }
+
+        public CurrencyAmount Divide(decimal divider)
+        {
+            return new CurrencyAmount { Currency = Currency, Amount = Amount / divider };
+        }
+
+        public CurrencyAmount ToUnitPrice(decimal quantity)
+        {
+            return new CurrencyAmount { Currency = Currency, Amount = Amount / quantity };
+        }
+
+        public CurrencyAmount Round()
+        {
+            return new CurrencyAmount { Currency = Currency, Amount = Math.Round(Amount, 2) };
+        }
+
+        public CurrencyAmount Clone()
+        {
+            return new CurrencyAmount { Currency = Currency, Amount = Amount };
+        }      
+    }
+}
---
+++ Common.Models/MonthPeriod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FluentChange.Extensions.Common.Models
+{
+    public class MonthPeriod
+    {
+        public int Year { get;private set; }
+        public int Month { get; private set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public int DaysInMonth { get; private set; }
+
+        public MonthPeriod()
+        {
+            
+        }
+        public MonthPeriod(int year, int month)
+        {
+            Year = year;
+            Month = month;
+            Start = new DateTime(Year, Month, 1, 0, 0, 0);
+            DaysInMonth = DateTime.DaysInMonth(Year, Month);
+            End = new DateTime(Year, Month, DaysInMonth, 23, 59, 59);
+        }
+    }
+}
