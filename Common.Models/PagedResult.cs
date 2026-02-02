51c0283e505327f66a9a7e04b01b74a9a3aab33b
Frank Mühlschlegel <frank@fluentchange.com>
2026-02-02 08:22:51 +0100

    pages result added

---
+++ Common.Models/PagedResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluentChange.Extensions.Common.Models
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => PageSize > 0 ? (int)Math.Ceiling((double)TotalCount / PageSize) : 1;
+        public bool HasPreviousPage => Page > 1;
+        public bool HasNextPage => Page < TotalPages;
+    }
+}
